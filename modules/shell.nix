4ab56ff3be7797bdab5ac9d8f15300055ef86055
Lucas JEANPIERRE <lulu.jeanpierre@gmail.com>
2026-08-06 19:56:40 +0200

    Update cat alias

--- modules/shell.nix
+++ modules/shell.nix
@@ -21,7 +21,7 @@
   environment.shellAliases = {
     ls = "eza --icons --group-directories-first";
     ll = "eza -la --icons --group-directories-first";
-    cat = "bat --style=auto";
+    cat = "bat --style=auto --theme='Catppuccin Macchiato' --paging=never --plain";
     nix-switch = "sudo nixos-rebuild switch --flake ~/nixos-conf#nixos";
     nix-update = "nix flake update --flake ~/nixos-conf && nix-switch";
     nix-clean = "sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +3 && nix-collect-garbage -d";
