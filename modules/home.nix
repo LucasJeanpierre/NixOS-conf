d6843ce86ba45f867794f3345c8ed38d9af46dce
Lucas JEANPIERRE <lulu.jeanpierre@gmail.com>
2026-08-06 20:36:48 +0200

    Typo

--- modules/home.nix
+++ modules/home.nix
@@ -8,29 +8,28 @@
   ];
 
   xdg.configFile."cosmic/com.system76.CosmicBackground/v1/all".text = ''
-      (
-          output: "all",
-          source: Path("/home/zeta/nixos-conf/assets/peaceful_mountain.png"),
-          filter_by_theme: true,
-          rotation_frequency: 300,
-          filter_method: Lanczos,
-          scaling_mode: Zoom,
-          sampling_method: Alphanumeric,
-      )
+    (
+      output: "all",
+      source: Path("/home/zeta/nixos-conf/assets/peaceful_mountain.png"),
+      filter_by_theme: true,
+      rotation_frequency: 300,
+      filter_method: Lanczos,
+      scaling_mode: Zoom,
+      sampling_method: Alphanumeric,
+    )
   '';
 
   xdg.configFile."cosmic/com.system76.CosmicTerm/v1/font_name".text = ''
-      "JetBrainsMono Nerd Font Mono"
+    "JetBrainsMono Nerd Font Mono"
   '';
 
   xdg.configFile."cosmic/com.system76.CosmicTerm/v1/show_headerbar".text = ''
-      false
+    false
   '';
 
   xdg.configFile."cosmic/com.system76.CosmicTheme.Mode/v1/is_dark".text = ''
     true
   '';
-
 
   catppuccin = {
     enable = true;
