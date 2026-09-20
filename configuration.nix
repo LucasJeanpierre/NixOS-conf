582cc0d0bea1615ab4799cad257bb053d49ca943
Lucas JEANPIERRE <lulu.jeanpierre@gmail.com>
2026-09-20 12:49:01 +0200

    sddm -> cosmic-greeter

--- configuration.nix
+++ configuration.nix
@@ -41,15 +41,7 @@
 
 
   # Enable the login manager
-  services.displayManager.cosmic-greeter.enable = false;
-
-  # Temp fix to cosmic greeter issue 184
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-  catppuccin.enable = true;
-
+  services.displayManager.cosmic-greeter.enable = true;
 
   # Enable the COSMIC desktop environment
   services.desktopManager.cosmic.enable = true;
